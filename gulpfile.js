8e7b6073c125151af0d2c881bb80db49bdc1a928
Gustavo Kreischer de Almeida <gustavokreischer@gmail.com>
2024-05-17 22:19:06 -0300

    add: gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,10 @@
-const { src, dest } = require("gulp");
-const sharpResponsive = require("gulp-sharp-responsive");
-const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
+import { src, dest } from 'gulp';
+import imagemin from 'gulp-imagemin';
 
-const compress = () =>
-  src("src/assets/images/*.{jpg,png}")
-    .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-    .pipe(
-      sharpResponsive({
-        formats: [
-          // jpeg
-          { width: 256, format: "jpeg", rename: { suffix: "-256" } },
-          { width: 512, format: "jpeg", rename: { suffix: "-512" } },
-          { width: 1024, format: "jpeg", rename: { suffix: "-1024" } },
-          // webp
-          { width: 256, format: "webp", rename: { suffix: "-256" } },
-          { width: 512, format: "webp", rename: { suffix: "-512" } },
-          { width: 1024, format: "webp", rename: { suffix: "-1024" } },
-          // avif
-          { width: 256, format: "avif", rename: { suffix: "-256" } },
-          { width: 512, format: "avif", rename: { suffix: "-512" } },
-          { width: 1024, format: "avif", rename: { suffix: "-1024" } },
-        ],
-      })
-    )
-    .pipe(dest("src/assets/compressed"));
+export default async (cb) => {
+  return src('src/assets/images/*.{jpg,png}')
+  .pipe(imagemin({verbose: true}))
+  .pipe(dest('src/assets/compressed2'));
 
-module.exports = {
-  compress,
-};
+  cb();
+}
